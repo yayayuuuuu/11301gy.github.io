d1bb8c32f9f30937fc8189398d2681f26dfc432b
yayayuuuuu <s111219031@stu.ntue.edu.tw>
2024-12-26 14:40:11 +0800

    1

--- about us/js/script.js
+++ about us/js/script.js
@@ -1,4 +1,16 @@
 // 讓 navbar 在滑動到頂部時固定
+function pieAnimation(){
+  let pie_el = document.querySelectorAll(".pie:not(.ani)");
+  pie_el.forEach((item, i) => {
+    //console.log( item.getBoundingClientRect().top - window.innerHeight );
+    // d: 螢幕的底部 到 圓圈的頂部 的距離
+    let d = item.getBoundingClientRect().top - window.innerHeight;
+    if(d < 0){
+      item.classList.add("ani");
+    }
+  });
+}
+
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
@@ -6,4 +18,8 @@ window.addEventListener('scroll', function () {
     } else {
       navbar.classList.remove('fixed-top');
     }
+
+    pieAnimation();
   });
+
+  
